18cc780ac036a1be82c08223897df00c048752e6
mikeappell <mikeappell@gmail.com>
2015-10-20 18:18:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(count)
+  count.downto(1) do |i|
+    puts "#{i} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(count)
+  count.downto(1) do |i|
+    puts "#{i} SECOND(S)!"
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
